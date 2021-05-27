1088839b66c61ef4e3d895ae1031f324321796b9
annguyen23 <54456351+annguyen23@users.noreply.github.com>
2021-05-27 03:14:58 -0700

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -5,11 +5,11 @@
 class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello, World !"); 
     }
     
     public static void printStarPiramid() {
-        System.out.println("    *");
+        System.out.println("    * ");
         System.out.println("   ***");
         System.out.println("  *****");
         System.out.println(" *******");
