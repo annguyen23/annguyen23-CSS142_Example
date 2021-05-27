ff981917ea7135016cbb6e996f6314ea59354756
annguyen23 <54456351+annguyen23@users.noreply.github.com>
2021-05-27 03:23:41 -0700

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -24,6 +24,13 @@ class HelloWorld {
         System.out.println(" +-----+ ");
     }
     
-    public static void printLongSentence() {
+    public static void printLongSentence() 
+    {
         System.out.println("This is a very long sentence that contains over 100 characters, and it should cause a warming in checkstyle!");
+    }
+    
+    public static void nothing(){
+        if (true) if (false) 
+            for (int i=0; i<10; i++) i++;
+    }
 }
