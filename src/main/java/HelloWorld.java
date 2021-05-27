7b84ad0c1ba5b3e14a82faa6760edbcc9419329d
annguyen23 <54456351+annguyen23@users.noreply.github.com>
2021-05-27 03:19:44 -0700

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -17,7 +17,7 @@ class HelloWorld {
     }
     
     public static void printFace() {
-        System.out.println(" +\"\"\"\"\"+ ");
+        System.out.println(" +\"\"\"\"\"+  ");
         System.out.println("[| o o |]");
         System.out.println(" |  ^  | ");
         System.out.println(" | '-' | ");
