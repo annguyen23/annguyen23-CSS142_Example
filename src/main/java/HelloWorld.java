c72717866c811d3f0ca1a9568c9c3b7212693005
annguyen23 <54456351+annguyen23@users.noreply.github.com>
2021-05-28 10:48:56 -0700

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -4,7 +4,7 @@
 
 class HelloWorld {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
         System.out.println("Hello, World! ") //an extra space was added in the end to make test case failed
     }
     
