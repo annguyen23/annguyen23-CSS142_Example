930a1a54ac3c3cc0a4215e8d2cf0dfa5431d3ba3
annguyen23 <54456351+annguyen23@users.noreply.github.com>
2021-06-23 02:01:57 -0700

    Update HW2Test.java

--- src/test/java/HW2Test.java
+++ src/test/java/HW2Test.java
@@ -106,6 +106,93 @@ public class HW2Test {
 	}
 	
 	
+	@Test
+	public void testProgramTwo() {
+    	HW2.runProgramTwo();
+        
+    	Assert.assertEquals("\nCheck carefully!" 
+			    + "One missing/extra character like space can make this test case failed\n"
+    			+ "If your program prints out similar Mario level, please don't worry if this test case fails!\n"
+			+ "This test case assume the Mario = \">-<0\" and Coin = \"@\", so don't worry if it fails due to different values!\n"
+    			+ "The runProgramOne() method should print out:"
+			    + "||" + "\n"
+			    + "||" + "\n"
+			    + "||" + "\n"
+			    + "||" + "\n"
+			    + "||" + "\n"
+			    + "||" + "\n\n"
+			    
+			    + "||" + "\n"
+			    + "||    |?|" + "\n"
+			    + "||" + "\n"
+			    + "||" + "\n"
+			    + "||    |?|" + "\n"
+			    + "||" + "\n\n"
+			    
+			    + "||" + "\n"
+			    + "||    |?|@" + "\n"
+			    + "||" + "\n"
+			    + "||" + "\n"
+			    + "||    |?|@" + "\n"
+			    + "||" + "\n\n"
+			    
+			    + "||" + "\n"
+			    + "||" + "\n"
+			    + "||@" + "\n"
+			    + "||@" + "\n"
+			    + "||@" + "\n"
+			    + "||" + "\n\n"			    
+			    
+			    + "||" +  "\n"
+			    + "||||" +  "\n"
+			    + "||||||" +  "\n"
+			    + "||||||||" +  "\n"
+			    + "||||||||||" +  "\n"
+			    + "||||||||||||>-<0" +  "\n"
+			    + "||" +  "\n"
+			    + "||              ^" +  "\n"
+			    + "|----------------" +  "\n"
+			    
+			    	,"||" + "\n"
+				+ "||" + "\n"
+				+ "||" + "\n"
+				+ "||" + "\n"
+				+ "||" + "\n"
+				+ "||" + "\n\n"
+					    
+			    + "||" + "\n"
+			    + "||    |?|" + "\n"
+			    + "||" + "\n"
+			    + "||" + "\n"
+			    + "||    |?|" + "\n"
+			    + "||" + "\n\n"
+			    
+			    + "||" + "\n"
+			    + "||    |?|@" + "\n"
+			    + "||" + "\n"
+			    + "||" + "\n"
+			    + "||    |?|@" + "\n"
+			    + "||" + "\n\n"
+			    
+			    + "||" + "\n"
+			    + "||" + "\n"
+			    + "||@" + "\n"
+			    + "||@" + "\n"
+			    + "||@" + "\n"
+			    + "||" + "\n\n"			    
+			    
+			    + "||" +  "\n"
+			    + "||||" +  "\n"
+			    + "||||||" +  "\n"
+			    + "||||||||" +  "\n"
+			    + "||||||||||" +  "\n"
+			    + "||||||||||||>-<0" +  "\n"
+			    + "||" +  "\n"
+			    + "||              ^" +  "\n"
+			    + "|----------------" +  "\n"
+					    , outputStreamCaptor.toString());
+	}
+	
 	@After
 	public void tearDown() {
     	System.setOut(standardOut);
