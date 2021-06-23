c9b13b37fde67697d3f1443c8b9253f5c848872b
annguyen23 <54456351+annguyen23@users.noreply.github.com>
2021-06-23 00:14:32 -0700

    Update and rename HelloWorld.java to HW2.java

---
+++ src/main/java/HW2.java
@@ -0,0 +1,69 @@
+package HW2;
+
+import java.util.Scanner;
+
+public class HW2 {
+    public static void main(String[] args) {
+    	runProgramOne();
+        runProgramTwo();
+    }
+    public static void runProgramOne() {
+    	segment1("");
+//    	segment2("");
+//    	segment2("");
+//    	segment1("");
+//    	segment3("");
+    }
+    public static void runProgramTwo() {
+    	// take user's inputs
+    	Scanner scan = new Scanner(System.in);
+    	System.out.println("What does Mario Look like?");
+    	String mario = scan.nextLine();
+    	System.out.println("What does a Coin look like?");
+    	String coin = scan.nextLine();
+    	
+    	// draw Mario Board 2
+    	segment1("");
+    	segment2("");
+    	segment2(coin);
+    	segment1(coin);
+    	segment3(mario);
+    }
+    
+    public static void segment1(String word) {
+    	System.out.println("||");
+    	System.out.println("||");
+    	System.out.println("||" + word);
+    	System.out.println("||" + word);
+    	System.out.println("||" + word);
+    	System.out.println("||");
+    	System.out.println();
+    }
+    
+    public static void segment2(String word) {
+    	System.out.println("||");
+    	System.out.println("||    |?|" + word);
+    	System.out.println("||");
+    	System.out.println("||");
+    	System.out.println("||    |?|" + word);
+    	System.out.println("||");
+    	System.out.println();
+    }
+    
+    public static void segment3(String word) {
+    	System.out.println("||");
+    	System.out.println("||||");
+    	System.out.println("||||||");
+    	System.out.println("||||||||");
+    	System.out.println("||||||||||");
+    	System.out.println("||||||||||||" + word);
+    	System.out.println("||");
+    	System.out.println("||              ^");
+    	System.out.println("|----------------");
+    }
+}
+
+
+
+
+
