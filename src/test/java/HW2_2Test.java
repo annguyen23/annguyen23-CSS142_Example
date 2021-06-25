0be469e65d249b731e7efcad10b06c52fcfe498f
annguyen23 <54456351+annguyen23@users.noreply.github.com>
2021-06-24 22:17:43 -0700

    Update HW2_2Test.java

--- src/test/java/HW2_2Test.java
+++ src/test/java/HW2_2Test.java
@@ -1,1 +1,19 @@
 
+import static org.junit.Assert.*;
+import static org.junit.contrib.java.lang.system.TextFromStandardInputStream.*;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.TextFromStandardInputStream;
+
+public class HW2_2Test {
+  @Rule
+  public final TextFromStandardInputStream systemInMock
+    = emptyStandardInputStream();
+
+  @Test
+  public void summarizesTwoNumbers() {
+    systemInMock.provideLines("1", "2");
+    assertEquals(3, Summarize.sumOfNumbersFromSystemIn());
+  }
+}
