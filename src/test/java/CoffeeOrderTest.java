7e8c0c8477465175e45429df5f6e2bbe0d083c87
annguyen23 <54456351+annguyen23@users.noreply.github.com>
2021-06-30 21:56:52 -0700

    Update CoffeeOrderTest.java

--- src/test/java/CoffeeOrderTest.java
+++ src/test/java/CoffeeOrderTest.java
@@ -52,4 +52,30 @@ public class CoffeeOrderTest {
                 + "Thank you for using CoffeeOrder 3000\n", 
                 outputStreamCaptor.toString());
     }
+    
+    @Test
+    public void test1drink_MochaGrande() {
+        systemInMock.provideLines("1", "3", "2");
+        CoffeeOrder.main(new String[] {});
+        Assert.assertEquals( "Check your outputs carefully. Any extra character "
+                + "like space can fail this test case.\n"
+                + "There should be 1 space  after (0-3): and you should print "
+                + "out new line using .println or at the end of each line."
+                + " This case tests 0 drink, so your program should print out:\n\n"
+                + "Welcome to CoffeeOrder 3000\n"
+                + "How many espresso drinks would you like today (0-3): \n"
+                + "Getting order for espresso drink #1\n"
+                + "What type (1: Americano, 2: Latte, 3: Mocha): \n"
+                + "What size (1: Tall, 2: Grande): \n"
+                + "Your total is 4.45"
+                + "Thank you for using CoffeeOrder 3000\n", 
+                "Welcome to CoffeeOrder 3000\n"
+                + "How many espresso drinks would you like today (0-3): 0\n"
+                + "Getting order for espresso drink #1\n"
+                + "What type (1: Americano, 2: Latte, 3: Mocha): \n"
+                + "What size (1: Tall, 2: Grande): \n"
+                + "Your total is 4.45"
+                + "Thank you for using CoffeeOrder 3000\n", 
+                outputStreamCaptor.toString());
+    }
 }
