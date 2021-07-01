ab5e6af6bda6614ef21a2412bce94547e93ef825
annguyen23 <54456351+annguyen23@users.noreply.github.com>
2021-07-01 00:04:46 -0700

    Update CoffeeOrderTest.java

--- src/test/java/CoffeeOrderTest.java
+++ src/test/java/CoffeeOrderTest.java
@@ -92,6 +92,41 @@ public class CoffeeOrderTest {
     }
     
     @Test
+    public void test2drink_LatteG_MochaG() {
+        systemInMock.provideLines("2", "2", "2", "3", "2");
+        CoffeeOrder.main(new String[] {});
+        Assert.assertEquals( "Check your outputs carefully. Any extra character "
+                + "like space can fail this test case.\n"
+                + "There should be 1 space  after (0-3): and you should print "
+                + "out new line using .println or at the end of each line."
+                + " This case tests 2 drink: Latte (size Grande) and "
+                + "Mocha (size Grande), so your program should print out:\n\n"
+                
+                + "Welcome to CoffeeOrder 3000\n"
+                + "How many espresso drinks would you like today (0-3): \n"
+                + "Getting order for espresso drink #1\n"
+                + "What type (1: Americano, 2: Latte, 3: Mocha): \n"
+                + "What size (1: Tall, 2: Grande): \n"
+                + "Getting order for espresso drink #2\n"
+                + "What type (1: Americano, 2: Latte, 3: Mocha): \n"
+                + "What size (1: Tall, 2: Grande): \n"
+                + "Your total is 8.40\n"
+                + "Thank you for using CoffeeOrder 3000\n\n", 
+                
+                "Welcome to CoffeeOrder 3000\n"
+                + "How many espresso drinks would you like today (0-3): \n"
+                + "Getting order for espresso drink #1\n"
+                + "What type (1: Americano, 2: Latte, 3: Mocha): \n"
+                + "What size (1: Tall, 2: Grande): \n"
+                + "Getting order for espresso drink #2\n"
+                + "What type (1: Americano, 2: Latte, 3: Mocha): \n"
+                + "What size (1: Tall, 2: Grande): \n"
+                + "Your total is 8.40\n"
+                + "Thank you for using CoffeeOrder 3000\n", 
+                outputStreamCaptor.toString());
+    }
+    
+    @Test
     public void test4drink() {
         boolean error = false;
         try {
